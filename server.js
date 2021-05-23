d0ba79f4302a6f4d24efd977b5d7797d54304cb2
Angie Valentina Córdoba <angievalentina1526@gmail.com>
2021-05-23 13:27:20 -0500

    heroku configuration

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/dist/bank-ui'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/src/index.html'));
+  res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
