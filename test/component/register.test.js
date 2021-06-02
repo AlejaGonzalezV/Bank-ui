2547deb47dd731cfb3c56a347a493e873129eacd
Angie Valentina Córdoba <angievalentina1526@gmail.com>
2021-06-01 22:31:13 -0500

    change package.json

--- test/component/register.test.js
+++ test/component/register.test.js
@@ -2,10 +2,20 @@ import '@testing-library/jest-dom/extend-expect'
 import { fireEvenet, render, screen } from '@testing-library/react'
 import React from 'react'
 import Home from '../../src/Pages/Home'
+import Add from '../../src/Pages/Add-user'
 
-test('When the user is in the main',()=>{
-    
-    render(<Home />);
-    expect(screen.getByRole('heading', { name: /lista de usuarios del banco/i })).toBeInTheDocument();
-    //expected(screen.getByRole('heading',{name: /lista de usuarios del banco/i})).toBeVisible();
+describe("Success cases, Add Dialog Form", () => {
+    test("When form is render title, buttons and required fields should be shown", () => { 
+        render(<Home />)
+        expect(screen.getByRole('heading', { name: /LISTA DE USUARIOS DEL BANCO/i })).toBeInTheDocument()
+     
+    });
+
+    test('The female option should be checked by default', () => {
+        render(<Add />);
+      
+        expect(screen.getByRole('button', { name: /AGREGAR USUARIO/i })).toBeInTheDocument();
+      
+      });
+
 });
